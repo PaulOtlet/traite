dd9d593f57cb35934ec7975f8f64ffb82e875ae9
czamb <czamb@users.noreply.github.com>
2014-12-30 18:13:28 +0100

    forgot to comment out the part about RTD theme

--- conf.py
+++ conf.py
@@ -110,17 +110,18 @@ pygments_style = 'sphinx'
 #html_theme = 'default'
 #html_theme = 'nature'
 
+# https://github.com/snide/sphinx_rtd_theme#using-this-theme-locally-then-building-on-read-the-docs
 # on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+#on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 #
-on_rtd = False
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    try:
-        import sphinx_rtd_theme
-        html_theme = 'sphinx_rtd_theme'
-        html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-    except:
-        html_theme = 'nature'
+#on_rtd = False
+#if not on_rtd:  # only import and set the theme if we're building docs locally
+#    try:
+#        import sphinx_rtd_theme
+#        html_theme = 'sphinx_rtd_theme'
+#        html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+#    except:
+#        html_theme = 'nature'
         
 
 # otherwise, readthedocs.org uses their theme by default, so no need to specify it
